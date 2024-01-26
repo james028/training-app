8b503eca976694fc3f51047eaf837dcc6e97fe70
Jakub Kania <kubak.15@gmail.com>
2024-01-26 12:12:38 +0100

    form in progress

--- src/components/Forms/AddTrainingForm/AddTrainingForm.tsx
+++ src/components/Forms/AddTrainingForm/AddTrainingForm.tsx
@@ -1,36 +1,67 @@
 import React from "react";
-import Modal from "../../shared/Modal/Modal";
-import EditTrainingForm from "../EditTrainingForm/EditTrainingForm";
 import SubmitButtons from "../SubmitButtons/SubmitButtons";
+import { useForm } from "react-hook-form";
+import FormInput from "../../shared/FormInput/FormInput";
 
+export type RegistrationFormFields = {
+  type: string;
+  bikeType?: string;
+  bikeKilometers?: string;
+  duration: string;
+};
 const AddTrainingForm = ({ closeModal }: any) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegistrationFormFields>();
+
+  const onSubmit = handleSubmit((data: any) => {
+    console.log("submitting...", data);
+  });
+
   return (
     <>
-      <div className="md:p-8 mb-2">
-        <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
-          <div className="md:col-span-5">
-            <label htmlFor="full_name">Typ treningu</label>
-            <input
-              type="text"
-              name="full_name"
-              id="full_name"
-              className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-              value=""
-            />
-          </div>
-          <div className="md:col-span-5">
-            <label htmlFor="full_name">Długość treningu</label>
-            <input
-              type="text"
-              name="full_name"
-              id="full_name"
-              className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-              value=""
-            />
-          </div>
-        </div>
-      </div>
-      <SubmitButtons closeModal={closeModal} />
+      <form onSubmit={onSubmit}>
+        <FormInput<RegistrationFormFields>
+          // @ts-ignore
+          id="type"
+          type="text"
+          name="type"
+          label="Type"
+          placeholder="Type"
+          className="mb-2"
+          register={register}
+          rules={{ required: "You must enter your first name." }}
+          errors={errors}
+        />
+        <SubmitButtons closeModal={closeModal} />
+        <button type={"submit"}>Add</button>
+      </form>
+      {/*<div className="md:p-8 mb-2">*/}
+      {/*  <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">*/}
+      {/*    <div className="md:col-span-5">*/}
+      {/*      <label htmlFor="full_name">Typ treningu</label>*/}
+      {/*      <input*/}
+      {/*        type="text"*/}
+      {/*        name="full_name"*/}
+      {/*        id="full_name"*/}
+      {/*        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"*/}
+      {/*        value=""*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*    <div className="md:col-span-5">*/}
+      {/*      <label htmlFor="full_name">Długość treningu</label>*/}
+      {/*      <input*/}
+      {/*        type="text"*/}
+      {/*        name="full_name"*/}
+      {/*        id="full_name"*/}
+      {/*        className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"*/}
+      {/*        value=""*/}
+      {/*      />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </>
   );
 };
---
+++ src/components/shared/FormInput/FormInput.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  DeepMap,
+  FieldError,
+  FieldValues,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+} from "react-hook-form";
+
+import { ErrorMessage } from "@hookform/error-message";
+import { Input } from "./Input/Input";
+
+export type FormInputProps<TFormValues extends FieldValues> = {
+  name: Path<TFormValues>;
+  rules?: RegisterOptions;
+  register?: UseFormRegister<TFormValues>;
+  errors?: Partial<DeepMap<TFormValues, FieldError>>;
+  className: string;
+};
+// & Omit<InputProps, "name">;
+export const FormInput = <TFormValues extends Record<string, unknown>>({
+  name,
+  register,
+  rules,
+  errors,
+  className,
+  ...props
+}: FormInputProps<TFormValues>): JSX.Element => {
+  // If the name is in a FieldArray, it will be 'fields.index.fieldName' and errors[name] won't return anything, so we are using lodash get
+  //const errorMessages = get(errors, name);
+  //const hasError = !!(errors && errorMessages);
+
+  return (
+    <div
+      // className={classNames("", className)}
+      aria-live="polite"
+    >
+      <Input
+        id={""}
+        label={""}
+        name={name}
+        //aria-invalid={hasError}
+        // className={classNames({
+        //   "transition-colors focus:outline-none focus:ring-2 focus:ring-opacity-50 border-red-600 hover:border-red-600 focus:border-red-600 focus:ring-red-600":
+        //     hasError,
+        // })}
+        {...props}
+        {...(register && register(name, rules))}
+      />
+      {/*{errors && <small className="error">{error.message}</small>}*/}
+      {/*{errors && <div className="error">This field is required</div>}*/}
+      {/*<ErrorMessage*/}
+      {/*  errors={errors}*/}
+      {/*  // eslint-disable-next-line @typescript-eslint/no-explicit-any*/}
+      {/*  name={name as any}*/}
+      {/*  render={({ message }) => (*/}
+      {/*    <FormErrorMessage className="mt-1">{message}</FormErrorMessage>*/}
+      {/*  )}*/}
+      {/*/>*/}
+    </div>
+  );
+};
+
+export default FormInput;
